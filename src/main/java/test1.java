acef3fbc3232476b69d9b502773ac181ad31f820
zzh <zzh2926742323@qq.com>
2023-07-08 17:19:41 +0800

    666

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -4,5 +4,7 @@ public class test1 {
         System.out.println("args = 2" );
         System.out.println("args = " );
         System.out.println("args = 4" );
+        System.out.println("args = " );
+        System.out.println("args = " );
     }
 }
